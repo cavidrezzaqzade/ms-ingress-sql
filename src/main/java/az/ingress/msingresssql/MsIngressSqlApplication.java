8e20502b8eec6f5c184d09ac42d4f3fa9fdb5a0a
cavidrezzaqzade <cavidrazzagzade@gmail.com>
2023-05-25 16:32:05 +0400

    feat: adding project to git

---
+++ src/main/java/az/ingress/msingresssql/MsIngressSqlApplication.java
@@ -0,0 +1,13 @@
+package az.ingress.msingresssql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsIngressSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsIngressSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/ingress/msingresssql/MsIngressSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ingress.msingresssql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsIngressSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
